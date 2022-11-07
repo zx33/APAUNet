246c3c272812d8a1fe14c732fbc02223a67d4fab
Yuncheng Jiang <55485168+Oliver-jiang@users.noreply.github.com>
2022-11-07 15:13:46 +0800

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -4,35 +4,26 @@
 import numpy as np
 import torch
 from medpy import metric
+import torch.nn.functional as F
+
 
 def load_dicefunc(output, target):
     smooth = 1e-4
     if torch.is_tensor(output):
         output = torch.nn.Softmax(dim=1)(output)
-
-
+        output = torch.argmax(output, 1)
         output = output.data.cpu().numpy()
 
     if torch.is_tensor(target):
         target = target.data.cpu().numpy()
-    dice = [[], [], []]
-    for i in range(output.shape[0]):
-        class0 = (output[i][0]>output[i][1]) & (output[i][0]>output[i][2]) 
-        class1 = (output[i][1]>output[i][0]) & (output[i][1]>output[i][2]) 
-        class2 = (output[i][2]>output[i][0]) & (output[i][2]>output[i][1])
-        intersection0 = (class0 & (target[i]==0)).sum()
-        intersection1 = (class1 & (target[i]==1)).sum()
-        intersection2 = (class2 & (target[i]==2)).sum()
 
-        union0 = (class0 | (target[i]==0)).sum()
-        union1 = (class1 | (target[i]==1)).sum()
-        union2 = (class2 | (target[i]==2)).sum()
-        cof0 = (intersection0 + smooth) / (union0 + smooth)
-        cof1 = (intersection1 + smooth) / (union1 + smooth)
-        cof2 = (intersection2 + smooth) / (union2 + smooth)
-        dice[0].append(2*cof0/(1+cof0))
-        dice[1].append(2*cof1/(1+cof1))
-        dice[2].append(2*cof2/(1+cof2))
+    dice = [[], [], []]
+    for idx in range(output.shape[0]):
+        for i in range(3):
+            intersection = ((output[idx] == i) & (target[idx] == i)).sum()
+            union = ((output[idx] == i) | (target[idx] == i)).sum()
+            cof = (intersection + smooth) / (union + smooth)
+            dice[i].append(2 * cof / (1 + cof))
     return np.asarray(dice)
 
 def load_hdfunc(outputs, targets):
